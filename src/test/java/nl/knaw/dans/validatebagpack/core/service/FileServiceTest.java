b2e11087a21818fc52c02fcd8742a91ad47818fb
Jan van Mansum <janvanmansum@gmail.com>
2025-11-26 15:08:19 +0100

    code cleanup

--- src/test/java/nl/knaw/dans/validatebagpack/core/service/FileServiceTest.java
+++ src/test/java/nl/knaw/dans/validatebagpack/core/service/FileServiceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.knaw.dans.validatebagpack.service;
+package nl.knaw.dans.validatebagpack.core.service;
 
 import nl.knaw.dans.validatebagpack.AbstractTestFixture;
 import nl.knaw.dans.validatebagpack.core.service.FileServiceImpl;
