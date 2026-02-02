59f62f9001ba03774bd9f5ef3f54b73411795299
jo-pol <jo-pol@users.noreply.github.com>
2026-02-02 15:36:02 +0100

    missing coverage

--- src/test/java/nl/knaw/dans/validatebagpack/core/rules/OaiOreAggregatedResourcesMustHaveRequiredPropsTest.java
+++ src/test/java/nl/knaw/dans/validatebagpack/core/rules/OaiOreAggregatedResourcesMustHaveRequiredPropsTest.java
@@ -15,13 +15,23 @@
  */
 package nl.knaw.dans.validatebagpack.core.rules;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import nl.knaw.dans.lib.util.ruleengine.RuleResult;
 import nl.knaw.dans.validatebagpack.AbstractTestFixture;
 import nl.knaw.dans.validatebagpack.core.service.FileServiceImpl;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.jspecify.annotations.NonNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +41,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OaiOreAggregatedResourcesMustHaveRequiredPropsTest extends AbstractTestFixture {
+    private ListAppender<ILoggingEvent> listAppender;
+    PrintStream originalErr = System.err;
+    ByteArrayOutputStream errContent;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        super.setUp();
+        errContent = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setErr(originalErr);
+    }
 
     @Test
     void validate_should_return_error_when_oai_ore_file_missing() throws Exception {
@@ -69,7 +94,72 @@ class OaiOreAggregatedResourcesMustHaveRequiredPropsTest extends AbstractTestFix
         assertThat(result.getErrorMessages().size()).isEqualTo(2);
         assertThat(result.getErrorMessages()).hasSameElementsAs(List.of(
             "(ii) Aggregated resource has missing 'name' property",
-            "(iii) Aggregated resource has missing 'restricted' property"));
+            "(iii) Aggregated resource has missing 'restricted' property")
+        );
+    }
+
+    @Test
+    void validate_ignores_aggregate_when_id_is_blank() throws Exception {
+        var oaiOre = testDir.resolve(OAI_ORE_PATH);
+        Files.createDirectories(oaiOre.getParent());
+        Files.writeString(oaiOre, """
+            {
+              "@context": {
+                "ore": "http://www.openarchives.org/ore/terms/",
+                "schema": "https://schema.org/",
+                "dvcore": "https://dataverse.org/schema/core#"
+              },
+              "@type": "ore:ResourceMap",
+              "ore:aggregates": [ {
+                    "@id": "   ",
+                    "schema:name": "Example Resource",
+                    "dvcore:restricted": false
+              } ]
+            }
+            """);
+        var rule = new OaiOreAggregatedResourcesMustHaveRequiredProps(getFileService());
+
+        var result = rule.validate(testDir);
+
+        assertEquals(RuleResult.Status.SUCCESS, result.getStatus());
+
+        // Check the error output
+        assertThat(errContent.toString()).contains("WARNING: Non well-formed subject [   ] has been skipped.");
+    }
+
+    @Test
+    void validate_should_throw_when_restricted_is_not_boolean() throws Exception {
+        var oaiOre = testDir.resolve(OAI_ORE_PATH);
+        Files.createDirectories(oaiOre.getParent());
+        Files.writeString(oaiOre, """
+            {
+              "@context": {
+                "ore": "http://www.openarchives.org/ore/terms/",
+                "schema": "https://schema.org/",
+                "dvcore": "https://dataverse.org/schema/core#"
+              },
+              "@type": "ore:ResourceMap",
+              "ore:aggregates": [ {
+                    "@id": "urn:example:xx",
+                    "schema:name": "Example Resource",
+                    "dvcore:restricted": "not a boolean"
+              },
+              {
+                    "@id": "urn:example:yy",
+                    "schema:name": "Example Resource",
+                    "dvcore:restricted": {}
+              } ]
+            }
+            """);
+        var rule = new OaiOreAggregatedResourcesMustHaveRequiredProps(getFileService());
+
+        var result = rule.validate(testDir);
+
+        assertEquals(RuleResult.Status.ERROR, result.getStatus());
+        assertThat(result.getErrorMessages()).hasSameElementsAs(List.of(
+            "(iii) Aggregated resource 'restricted' property is not a boolean",
+            "(iii) Aggregated resource 'restricted' property is not a literal")
+        );
     }
 
     @Test
